18666c594cf97c6059609917a9c3ca2061841934
wendikardian <wendikardian@gmail.com>
2024-08-27 19:43:33 +0700

    updates

---
+++ BlogWeb/script.js
@@ -0,0 +1,10 @@
+function changeText() {
+    var replaceText = document.    
+    getElementsByClassName("mini-text");
+    replaceText[0].innerHTML = "scroll untuk melihat";
+ 
+    document.getElementId("icon-up").style.display = "none";
+ 
+    document.getElementId("icon-down").style.display = "block";
+ 
+ }
