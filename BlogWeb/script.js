a1f79181ba8fedbff7ac1dab68e0aa8dd07974d2
wendikardian <wendikardian@gmail.com>
2024-08-29 19:00:59 +0700

    update

--- BlogWeb/script.js
+++ BlogWeb/script.js
@@ -1,10 +1,6 @@
 function changeText() {
-    var replaceText = document.    
-    getElementsByClassName("mini-text");
+    var replaceText = document.getElementsByClassName("mini-text");
     replaceText[0].innerHTML = "scroll untuk melihat";
- 
     document.getElementId("icon-up").style.display = "none";
- 
     document.getElementId("icon-down").style.display = "block";
- 
  }
